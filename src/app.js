dcbec2bc8d7d6aed6a81eec312505cf2b0b4405e
Shreya Chopra <chopra.shreya03@gmail.com>
2026-04-20 19:57:37 +0530

    feat: create instance a new instance of the User model

--- src/app.js
+++ src/app.js
@@ -1,60 +1,36 @@
 const express = require('express');
+const connectDB = require('./config/database');
 const app = express();
-require('./config/database');
+const User = require('./models/user');
 
-const { adminAuthorization, userAuth } = require("../middlewares/auth");
-
-// We can also wrap the routes inside an array, it will make no difference.
-// app.use("/route", [rH, rH2, rH3, rH4, rH5]);
-
-// GET /users => middleware => request handler
-
-// Handle Auth middleware for all GET, POST...requests
-app.use("/admin", adminAuthorization);
-
-app.use("/user/login", (req, res) => {
-    res.send("User logged in successfully...");
-});
-
-app.use("/user" , userAuth, (req, res, next) => {
-    res.send("user data sent");
-    next();
-});
-
-app.get("/admin/getAllData", (req, res, next) => {
-        res.send("All data sent");
-        next();
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-        res.send("delete the user");
-});
-
-app.use("/", (err, req, res, next) => {
-    if(err) {
-        // log your error
-        res.status(500).send("Something went wrong");
-    }
-});
-
-app.get("/getUserData", (req, res) => {
+app.post("/signup", async(req, res) => {
+    // Creating a new instance of the User model
+    const user = new User({
+        firstName: "Shreya",
+        lastName: "Chopra",
+        emailId: "shreya050@gmail.com",
+        password: "joabdhsbshs",
+    });
+    
+    // Always use try catch block while handling DB
     try{
-        // logic of DB call and get user data
-        throw new Error("dhddhhdhdhdh");
-        res.send("User Data Sent");
-    }
-    catch (err) {
-        res.status(500).send("Some error ,contact support team");
+    await user.save();
+    res.send("User added successfully....");
+    } catch (err) {
+        res.status(400).send("error saving the user:" + err.message);
     }
 });
 
-app.use("/", (err, req, res, next) => {
-    if(err) {
-        // log your error
-        res.status(500).send("Something went wrong");
-    }
-});
+connectDB()
+    .then(() => {
+        console.log("Connection was established successfully...")
+        app.listen(7777, () => {
+        console.log("Server is listening on port 7777...")
+        });
+    })
+    .catch((err) => {
+        console.err("Connection wasn't established successfully...")
+    });
+    
 
-app.listen(7777, () => {
-    console.log("Server is listening on port 7777...")
-});
+
