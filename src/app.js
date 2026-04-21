9ae48b6fe0662d20a21bc82a16a331ce5f02ce21
Shreya Chopra <chopra.shreya03@gmail.com>
2026-04-21 18:46:20 +0530

    feat: get user by email

--- src/app.js
+++ src/app.js
@@ -20,6 +20,34 @@ app.post("/signup", async(req, res) => {
     }
 });
 
+// Get user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try {
+    const users = await User.find({ emailId: userEmail });
+        if(users.length === 0) {
+            res.status(404).send("User not found");
+        } else{
+            res.send(users);
+        }
+     } 
+        catch(err) {
+        res.status(400).send("something went wrong");
+    }
+});
+
+// Feed API - GET /feed - get all the users from the DB
+app.get("/feed", async (req, res) => {
+
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
 connectDB()
     .then(() => {
         console.log("Connection was established successfully...")
