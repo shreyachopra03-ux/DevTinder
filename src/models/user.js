676d92c518f0e0b2dd3e937b3a329c42d0781327
Shreya Chopra <chopra.shreya03@gmail.com>
2026-04-22 02:32:00 +0530

    feat: add validation and sanitation flags to the user schema

--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,56 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 50,
+        trim: true
     },
     lastName: {
         type: String,
     },
     emailId: {
         type: String,
-    },
-    gender: {
-        type: String,
+        trim: true,
+        required: true,
+        lowercase: true,
+        unique: true
     },
     password: {
         type: String,
+        required: true,
+        minLength: 8
     },
     age: {
         type: Number,
+        required: true,
+        min: 18
+    },
+    gender: {
+        type: String,
+        required: true,
+        validate(value) {
+            if(!["male", "female", "others"].includes(value)) {
+                throw new Error("Gender data is not valid");
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        required: true,
+        default: "https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png"
+    },
+    skills: {
+        type: [String]
+    },
+    about: {
+        type: String,
+        default: "This is user's default about",
+        maxLength: 500
     }
-});
+}, {
+    timestamps: true
+   }
+);
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
