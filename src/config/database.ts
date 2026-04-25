a279069713c9daf179f211306aab65c25c3cbaaf
Shreya Chopra <chopra.shreya03@gmail.com>
2026-04-26 01:38:58 +0530

    refactor: minor code cleanup

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 require('dotenv').config();
 
 const connectDB = async() => {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI!);
 };
 
 export default connectDB;
