790399d5b5d3614888eaa85b98b18f6eb1f940d7
Shreya Chopra <chopra.shreya03@gmail.com>
2026-04-26 00:43:26 +0530

    feat: setup modular routing and server initialization with DB connection

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,30 @@
-const express = require('express');
+import express from 'express';
 import connectDB from './config/database';
 const app = express();
-const PORT = process.env.PORT || 7777;
-const cookieParser = require("cookie-parser");
+const PORT: number = Number(process.env.PORT) || 7777;
+import cookieParser from "cookie-parser";
+import authRouter from './routes/auth';
+import profileRouter from './routes/profile';
+import requestRouter from './routes/request';
 
-// Middleware which converts json data into js object
+// Middleware 
 app.use(express.json());
-
-// Middleware which parses the cookie
 app.use(cookieParser());
 
-const authRouter = require('./routes/auth');
-const profileRouter  = require('./routes/profile')
-const requestRouter = require('./routes/request');
-
-// Routes
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 
-function startServer(port) {
+function startServer(port: number) {
     const server = app.listen(port, () => {
         console.log(`Server is listening on port ${port}`);
     });
 
-    server.on('error', (err) => {
+    server.on('error', (err: any) => {
         if (err.code === 'EADDRINUSE') {
+            const nextPort = port + 1;
             console.log(`Port ${port} is already in use..., so trying ${port + 1}...`);
-            startServer(Number(port) + 1); 
+            startServer(nextPort); 
         } else {
             console.error("Server error:", err);
         }
@@ -43,3 +40,4 @@ connectDB()
         console.error("Database connection failed!");
         process.exit(1); 
     });
+
