f3e4c4d7e7ffcf8e3896cf045835671a2dab9ef8
Shreya Chopra <chopra.shreya03@gmail.com>
2026-04-26 00:53:16 +0530

    feat: add connection request route with auth middleware

--- src/routes/request.ts
+++ src/routes/request.ts
@@ -1,10 +1,11 @@
-const express = require('express');
+import express from 'express';
 const requestRouter = express.Router();
-const { userAuth } = require('../middlewares/auth');
+import userAuth from '../middlewares/auth';
+import type { Request, Response } from "express";
 
-requestRouter.post("/sendConnectionRequest", userAuth, async(req, res) => {
+requestRouter.post("/sendConnectionRequest", userAuth, async (req: any, res: Response) => {
     const user = req.user;
     res.send(user.firstName + " sent the connection request");
 });
 
-module.exports = requestRouter;
+export default requestRouter;
