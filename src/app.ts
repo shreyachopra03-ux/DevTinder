160fdbcdce9bad18082742a45e9b8c504396d3b3
Shreya Chopra <chopra.shreya03@gmail.com>
2026-04-26 12:48:02 +0530

    refactor: minor code cleanup

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import authRouter from './routes/auth';
 import profileRouter from './routes/profile';
 import requestRouter from './routes/request';
 
-// Middleware 
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 
