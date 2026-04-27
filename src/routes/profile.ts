43269dfd6ac9fafe09546f4409ef87fa232c33e1
Shreya Chopra <chopra.shreya03@gmail.com>
2026-04-27 12:00:00 +0530

    refactor: minor code cleanup

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -2,6 +2,11 @@ import express from 'express';
 const profileRouter = express.Router();
 import userAuth from '../middlewares/auth.js';
 import type { Request, Response } from "express";
+import { validateEditProfile } from "../utils/validation.js";
+// import bcrypt from "bcrypt";
+// import User from "../models/user.js";
+import crypto from "node:crypto";
+// import jwt from "jsonwebtoken";
 
 interface IUser {
     firstName: string;
@@ -10,13 +15,16 @@ interface IUser {
     password: string;
     gender: string
     age: number;
+    passwordResetToken?: string;
+    resetExpireTime?: number;
+    save?: 
 }
 
 interface AuthRequest extends Request {
     user?: IUser;
 }
 
-profileRouter.get("/profile", userAuth, async(req: AuthRequest, res: Response) => {
+profileRouter.get("/profile/view", userAuth, async(req: AuthRequest, res: Response) => {
 
     try{
         const user = req.user;
@@ -26,4 +34,53 @@ profileRouter.get("/profile", userAuth, async(req: AuthRequest, res: Response) =
     }
 });
 
+profileRouter.post("/profile/edit", userAuth, async (req: AuthRequest, res: Response) => {
+
+    try {
+    validateEditProfile(req);
+
+    const loggedInUser = req.user;
+    // console.log(loggedInUser);
+
+    Object.keys(req.body).forEach((key) => {
+        (loggedInUser as any)[key] = req.body[key];
+    });
+
+    await(loggedInUser as any).save();
+
+    res.json({
+        message: `${loggedInUser?.firstName}, your profile was updated successfully`,
+        data: loggedInUser
+    });
+    } catch (err: any) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+});
+
+profileRouter.patch("/profile/password", userAuth, async(req: AuthRequest, res: Response) => {
+    try {
+        const user = req.user;
+        if(!user) {
+            throw new Error("User not found");
+        }
+
+        // For generating a secure random token 
+        const resetToken = crypto.randomBytes(20).toString("hex");
+        console.log(resetToken);
+
+        user.passwordResetToken = resetToken;
+        user.resetExpireTime = Date.now() + 15 * 60 * 1000;
+
+        await user.save();
+
+        // Send reset password mail to the user
+        res.send("https://yourfrontend.com/reset-password?token=token");
+
+    } catch (err: any) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+});
+
 export default profileRouter;
+
+
