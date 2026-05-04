b3f33d814b453e6e76ea675826a5a072718ac01d
Shreya Chopra <chopra.shreya03@gmail.com>
2026-05-04 18:08:50 +0530

    feat: create API for sending connection request to any user on the platform

--- src/routes/request.ts
+++ src/routes/request.ts
@@ -2,6 +2,7 @@ import express from 'express';
 const requestRouter = express.Router();
 import userAuth from '../middlewares/auth.js';
 import type { Request, Response } from "express";
+import ConnectionRequest from "../models/connectionRequest.js";
 
 interface IUser {
     firstName: string;
@@ -10,15 +11,41 @@ interface IUser {
     password: string;
     gender: string
     age: number;
+    _id: string;
+    status?: string;
 }
 
 interface AuthRequest extends Request {
     user?: IUser;
 }
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req: AuthRequest, res: Response) => {
-    const user = req.user;
-    res.send(user?.firstName + " sent the connection request");
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req: AuthRequest, res: Response) => {
+
+    try {
+        const fromUserId = req.user?._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status as string;
+
+        const allowedStatus = ["ignored", "interested"];
+        if(!allowedStatus.includes(status)) {
+            return res
+            .status(400)
+            .json({ message: "Invalid status type : " + status });
+        }
+
+        const connectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status
+        });
+
+        const data = await connectionRequest.save();
+        res.json({
+            message: `${status}`
+        });
+    } catch (err: any) {
+        res.status(400).send("ERROR : " + err.message)
+    }
 });
 
 export default requestRouter;
