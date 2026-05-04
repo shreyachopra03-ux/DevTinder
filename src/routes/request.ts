3271847900b09f65e93149b0486bc69524b208c6
Shreya Chopra <chopra.shreya03@gmail.com>
2026-05-04 20:21:46 +0530

    feat: add corner case to handle the user not found

--- src/routes/request.ts
+++ src/routes/request.ts
@@ -3,6 +3,7 @@ const requestRouter = express.Router();
 import userAuth from '../middlewares/auth.js';
 import type { Request, Response } from "express";
 import ConnectionRequest from "../models/connectionRequest.js";
+import User from "../models/user.js";
 
 interface IUser {
     firstName: string;
@@ -26,11 +27,31 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req: Auth
         const toUserId = req.params.toUserId;
         const status = req.params.status as string;
 
+        // 1st corner case
         const allowedStatus = ["ignored", "interested"];
         if(!allowedStatus.includes(status)) {
             return res
             .status(400)
             .json({ message: "Invalid status type : " + status });
+        }
+
+        // 2nd corner case
+        const toUser = await User.findById(toUserId);
+        if (!toUser) {
+            return res.status(404).json({ message: "User not found !!"});
+        }
+
+        // 3nd corner case
+        const existingConnectionRequest = await ConnectionRequest.findOne({
+            // It's a mongodb query which means that if the user A has already send connection req to user B or user B has already sent connection req to user A 
+            // so in both the cases, duplicate connection request error will come. 
+            $or: [
+                { fromUserId, toUserId },
+                { fromUserId: toUserId, toUserId: fromUserId },
+            ]
+        });
+        if(existingConnectionRequest) {
+            return res.status(400).json({ message: "Connection request already exists !!"})
         }
 
         const connectionRequest = new ConnectionRequest({
@@ -41,7 +62,8 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req: Auth
 
         const data = await connectionRequest.save();
         res.json({
-            message: `${status}`
+            message: `${status}`,
+            data
         });
     } catch (err: any) {
         res.status(400).send("ERROR : " + err.message)
