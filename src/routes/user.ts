37db089aeedf3b40a1749d7dfa0d7df3e5f921ba
Shreya Chopra <chopra.shreya03@gmail.com>
2026-05-06 00:05:07 +0530

    refactor: minor code cleanup

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -27,17 +27,18 @@ userRouter.get("/user/requests/received", userAuth, async(req: AuthRequest, res:
         if(!loggedInUser) {
         return res.status(401).send("Please Login!");
         }
-        const connectionRequest = await ConnectionRequest.find({
-            toUserId: loggedInUser._id,
-            status: "interested",
-        }).populate("fromUserId", ["firstName", "lastName"]);
+       
+        const connectionRequests = await ConnectionRequest.find({
+        toUserId: loggedInUser._id,
+        status: "interested",
+        }).populate("fromUserId", ["firstName", "lastName", "age", "gender", "skills", "about", "photoUrl"]);
 
         res.json({
-            message: "Data fetched successfully !!", 
-            data: connectionRequest
+            message: "Data fetched successfully !!",
+            data: connectionRequests 
         });
     } catch (err: any) {
-        return res.send(400).send("ERROR : " + err.message)
+        return res.status(400).json("ERROR : " + err.message)
     }
 });
 
