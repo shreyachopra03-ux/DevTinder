0c0ada413a68f157d0ea1da942d7e50a019b50fe
Shreya Chopra <chopra.shreya03@gmail.com>
2026-05-05 13:47:06 +0530

    refactor: minor code cleanup

--- src/models/connectionRequest.ts
+++ src/models/connectionRequest.ts
@@ -22,6 +22,15 @@ const connectionRequestSchema = new mongoose.Schema({
     timestamps: true
 });
 
+connectionRequestSchema.pre("save", function (next: any) {
+  const connectionRequest = this;
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+     // check if fromUserId equals to toUserId
+     return next(new Error("Cannot send connection request to yourself !!"));
+  }
+  next();
+});
+
 const ConnectionRequest = mongoose.model("ConnectionRequest", connectionRequestSchema);
 
 export default ConnectionRequest;
