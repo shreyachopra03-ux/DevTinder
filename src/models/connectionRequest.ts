635ffa763a73d3744677bfb3525e62de1293224e
Shreya Chopra <chopra.shreya03@gmail.com>
2026-05-05 23:11:40 +0530

    feat: add ref for populating the user.ts

--- src/models/connectionRequest.ts
+++ src/models/connectionRequest.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connectionRequestSchema = new mongoose.Schema({
     fromUserId: {
         type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
         required: true
     },
     toUserId: {
