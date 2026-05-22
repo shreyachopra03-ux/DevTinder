0c15a24b49b9534df6909459e793ed1f22909238
Shreya Chopra <chopra.shreya03@gmail.com>
2026-05-23 01:15:01 +0530

    update: install cors package

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,14 @@ import authRouter from './routes/auth.js';
 import profileRouter from './routes/profile.js';
 import requestRouter from './routes/request.js';
 import userRouter from "./routes/user.js";
+import cors from "cors";
 
 // Middlewares
+app.use(cors({
+    origin: "http://localhost:5173/",
+    credentials: true
+})
+);
 app.use(express.json());
 app.use(cookieParser());
 
