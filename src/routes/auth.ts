f6d9cff9dca4d86f310e393a72ad17d243305afc
Shreya Chopra <chopra.shreya03@gmail.com>
2026-05-23 13:33:42 +0530

    refactor: minor code cleanup

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -67,7 +67,7 @@ authRouter.post("/login", async(req: Request , res: Response) => {
 
             // Add the token to cookie and send the response back to the user
             res.cookie("token", token, { expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)});
-            res.send("Login successful");
+            res.send(user);
 
         } else {
             throw new Error("Invalid credentials");
