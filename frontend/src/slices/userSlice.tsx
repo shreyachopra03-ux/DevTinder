2f5e2b565f2c756e7f9bc9b1a1e9048934e17a06
Shreya Chopra <chopra.shreya03@gmail.com>
2026-06-08 14:02:51 +0530

    chore: add user slice

--- frontend/src/slices/userSlice.tsx
+++ frontend/src/slices/userSlice.tsx
@@ -1,15 +1,26 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
 interface UserState {
-
+    id: string,
+    name?: string,
+    email: string
 }
 
 const initialState: UserState | null = null as UserState | null;
 
 const userSlice = createSlice({
     name: "user",
-    initialState: null,
+    initialState,
     reducers: {
-
+        addUser: (_state, action: PayloadAction<UserState>) => {
+            return action.payload;
+        },
+        deleteUser: () => {
+            return null;
+        }
     }
-})
+});
+
+export const { addUser,deleteUser } = userSlice.actions;
+
+export default userSlice.reducer;
