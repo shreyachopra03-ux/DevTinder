f8c17143dce1ca93fd6880788e167dd86e3cfcef
Shreya Chopra <chopra.shreya03@gmail.com>
2026-06-08 21:04:51 +0530

    refactor: minor code cleanup

--- frontend/src/Body.tsx
+++ frontend/src/Body.tsx
@@ -1,8 +1,37 @@
 import NavBar from "./components/NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./components/Footer";
+import { useDispatch } from "react-redux";
+import { BASE_URL } from './utils/constants';
+import { addUser } from "./slices/userSlice";
+import axios from "axios";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUser = async() => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true
+      });
+      dispatch(addUser(res.data));
+
+    } catch (err) { 
+        if (axios.isAxiosError(err)) {
+            if (err.response?.status === 401) { 
+              navigate("/login");
+            }
+        } else {
+           console.error("Non Axios error occurred:", err);
+        }
+    }};
+
+    useEffect(() => {
+      fetchUser();
+    }, [])
+
   return (
     <div className="min-h-screen bg-base-100">
       <NavBar />
