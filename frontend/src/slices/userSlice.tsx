6cd1ca1d6684320b04d956effbd51374da574614
Shreya Chopra <chopra.shreya03@gmail.com>
2026-06-08 12:38:29 +0530

    chore: create user slice

---
+++ frontend/src/slices/userSlice.tsx
@@ -0,0 +1,15 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+interface UserState {
+
+}
+
+const initialState: UserState | null = null as UserState | null;
+
+const userSlice = createSlice({
+    name: "user",
+    initialState: null,
+    reducers: {
+
+    }
+})
