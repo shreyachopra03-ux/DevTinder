6b24395f12984baa59f864a61b417e8ad8017fa4
Shreya Chopra <chopra.shreya03@gmail.com>
2026-08-04 23:19:16 +0530

    refactor: minor code cleanup

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,13 @@ import "dotenv/config";
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI!);
+    try {
+        await mongoose.connect(process.env.MONGO_URI!);
+    } catch (err) {
+        console.error("MongoDB connection error:", JSON.stringify(err, null, 2));
+        console.error("Error details:", err instanceof Error ? err.message : String(err));
+        throw err;
+    }
 };
 
 export default connectDB;
